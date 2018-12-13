dc965224ac6432ddf79f5718e6be1656cb2a6534
Manchangdx <1195581533@qq.com>
2018-12-13 18:48:15 +0800

    第 4 章：Web 表单

--- hello.py
+++ hello.py
@@ -1,8 +1,18 @@
 from flask import Flask, request, render_template
+from flask_moment import Moment
 from flask_bootstrap import Bootstrap
+from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
+
+class NameForm(FlaskForm):
+    name = StringField('What\'s your name?', validators=[DataRequired()])
+    submit = SubmitField('提交')
 
 @app.route('/')
 def index():
@@ -11,7 +21,16 @@ def index():
 @app.route('/用户/<username>')
 def user(username):
     ua = request.headers.get('User-Agent')
-    return render_template('user.html', username=username, ua=ua)
+    return render_template('user.html', username=username, ua=ua,
+        current_time=datetime.utcnow())
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def interval_server_error(e):
+    return render_template('500.html'), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
