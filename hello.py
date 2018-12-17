9e4c3970992cb29db1309b087d07ddc4b4297429
Manchangdx <1195581533@qq.com>
2018-12-17 17:46:34 +0800

    第 5 章：数据库

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, session, redirect, url_for, flash
 from flask_moment import Moment
 from flask_bootstrap import Bootstrap
 from datetime import datetime
@@ -7,6 +7,7 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'HELLO'
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
@@ -14,11 +15,17 @@ class NameForm(FlaskForm):
     name = StringField('What\'s your name?', validators=[DataRequired()])
     submit = SubmitField('提交')
 
-@app.route('/')
+@app.route('/', methods=['get', 'post'])
 def index():
-    return render_template('index.html')
+    form = NameForm()
+    if form.validate_on_submit():
+        form=form
+        session['name'] = form.name.data
+        flash('看起来你已经更名为 {}'.format(form.name.data), 'success')
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'))
 
-@app.route('/用户/<username>')
+@app.route('/user/<username>')
 def user(username):
     ua = request.headers.get('User-Agent')
     return render_template('user.html', username=username, ua=ua,
