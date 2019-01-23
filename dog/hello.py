3645cc344d0d5c82550c3c84aa4444e80b520905
Manchangdx <1195581533@qq.com>
2019-01-23 12:17:45 +0800

    3.6 使用 Flask-Moment

--- dog/hello.py
+++ dog/hello.py
@@ -1,14 +1,25 @@
 from flask import Flask, render_template, request
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 @app.route('/')
 def index():
     ua = request.headers.get('User-Agent')
-    return render_template('index.html', ua=ua)
+    return render_template('index.html', ua=ua, current_time=datetime.utcnow())
 
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html', name=name)
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(error):
+    return render_template('500.html'), 500
